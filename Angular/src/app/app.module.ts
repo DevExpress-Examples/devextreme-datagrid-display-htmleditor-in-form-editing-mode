035a651e8f50591525a1c4c2d14d1debe8bf0904
LexDX <42230789+LexDX@users.noreply.github.com>
2021-02-11 20:43:46 +0300

    fix imports

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { DxDataGridModule } from 'devextreme-angular';
+import { DxDataGridModule } from 'devextreme-angular/ui/data-grid';
 
 import { AppComponent } from './app.component';
 
