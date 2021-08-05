3411d4310728032e899eb336b3c3be03d0cc65f3
DevExpressExampleBot <fakeEmail>
2021-08-05 19:03:20 +0300

    Source auto update [skip ci]

---
+++ React/build/precache-manifest.6b69107e8ad405ef4915ea04003b9b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda8438e735e53690a15dfc8b38b15d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c533519219f2b83c73c",
+    "url": "/static/css/main.b310fb38.chunk.css"
+  },
+  {
+    "revision": "35b8e7eed7a3f076947f",
+    "url": "/static/js/2.60fdfbea.chunk.js"
+  },
+  {
+    "revision": "27e38fc76bc3ccf4183d7365592804e1",
+    "url": "/static/js/2.60fdfbea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c533519219f2b83c73c",
+    "url": "/static/js/main.4a0b72bf.chunk.js"
+  },
+  {
+    "revision": "d1d36571adbc72cb47f9",
+    "url": "/static/js/runtime-main.0135710a.js"
+  }
+]);
